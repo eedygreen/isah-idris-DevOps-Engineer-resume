bcef258d438cc2232364b105d81167886490d143
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-08-09 22:23:32 +0100

    fix typo

--- src/main/java/resume/resume.java
+++ src/main/java/resume/resume.java
@@ -1,5 +1,5 @@
 package resume;
-    public class resume {
+    public class Resume {
         public static void main (String args []) {
             System.out.println("Here is my resume");
         }
