a192905594893cdd2f0bc2653334d1a386c59e60
Isah Idris <58168886+eedygreen@users.noreply.github.com>
2022-08-10 23:26:58 +0100

    example

--- src/main/java/resume/resume.java
+++ src/main/java/resume/resume.java
@@ -1,6 +1,6 @@
 package resume;
-    public class Resume {
-        public static void main(String args[]) {
-            System.out.println("Here is my application for cloud-infra-engineer position!");
+    class Resume {
+        public static void main(String[] args) {
+            System.out.println("cloud-infra-engineer");
         }
     }
